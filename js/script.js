365c77c8a7684a2e26c0b536319633d1fffc9547
lindam <linda.minotti2@gmail.com>
2020-06-15 20:07:07 +0200

    Aggiunto tasto per cancellare le cose della lista fatte: tick.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+  // Array con la lista delle cose da fare
   var arrayCoseDaFare = [
     "Doccia",
     "Esercizi",
@@ -7,10 +7,24 @@ $(document).ready(function() {
   ];
   console.log(arrayCoseDaFare);
 
+  // Ciclo per popolare la lista nel DOM con la lista dell'array
   for (var i = 0; i < arrayCoseDaFare.length; i++) {
     var cloneLi = $('.template li').clone();
-    cloneLi.text(arrayCoseDaFare[i]);
+    cloneLi.prepend(arrayCoseDaFare[i]);
     $('.todo-lista ul').append(cloneLi);
   }
+
+  // Tasto per rimuovere le cose già fatte (cliccare sui tick)
+  // $('.delete-item').click(function(){
+  //   $(this).parent().remove();
+  // });
+
+  // Per essere sicuri che funziona anche con gli elementi che creo
+  // tramite un prompt
+  // il tasto rimuovi lo uso con la funzione .on, forma estesa di click
+  $(document).on('click','.delete-item', function(){
+    $(this).parent().remove();
+  });
+
 
 });
